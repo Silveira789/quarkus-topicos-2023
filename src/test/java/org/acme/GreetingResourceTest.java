a750c123b6aa3619d0de64a7fda5bb31b2ae614c
Guilherme Silveira <guilhermesf1210@gmail.com>
2023-08-26 00:04:23 -0300

    Atualização: Atividade 4 concluida

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
-public class GreetingResourceTest {
+// @QuarkusTest
+// public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+//     @Test
+//     public void testHelloEndpoint() {
+//         given()
+//           .when().get("/estados")
+//           .then()
+//              .statusCode(200)
+//              .body(is("Hello RESTEasy"));
+//     }
 
-}
+// }
+
+
